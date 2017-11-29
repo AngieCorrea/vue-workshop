448aab8815a45a7d1844e7b1d35bbda912ac53a7
Javier Diaz <coderdiaz@gmail.com>
2017-11-29 14:13:22 -0600

    Added view to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+// Views
+import HomeView from '@/views/Home/Home'
+
 Vue.use(Router)
 
 export default new Router({
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: HomeView
+    }
+  ]
 })
