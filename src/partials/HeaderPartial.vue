cc8615d150a2dfe0bcf0501ad1cb95214bac5849
Javier Diaz <coderdiaz@gmail.com>
2017-11-29 14:12:44 -0600

    Added header partial

---
+++ src/partials/HeaderPartial.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="header">
+    <div class="container">
+      Pokédex
+    </div>
+  </header>
+</template>
+<script>
+export default {
+  name: 'HeaderPartial'
+}
+</script>
