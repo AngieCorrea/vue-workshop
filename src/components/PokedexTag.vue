b9067d6510855bfed0968e4b70fa85277a5d0057
Javier Diaz <coderdiaz@gmail.com>
2017-11-29 15:56:14 -0600

    Added footer partial and pokedex tag component

---
+++ src/components/PokedexTag.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: 'PokedexTag',
+  props: {
+    pokemonType: {
+      type: String,
+      required: true
+    }
+  },
+  render (h) {
+    return <span class="pokedex-tag badge badge-primary mr-1">
+      {this.pokemonType}
+    </span>
+  }
+}
+</script>
